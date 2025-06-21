fe78bc45c701166ef39f79d17910824e9a6c4a40
Dhruvik Limbasiya <dhruvikpatel089@gmail.com>
2025-06-21 16:04:50 +0530

    changes in footer and changes in nacklaces image

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr'
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-contact-us',
@@ -9,23 +10,46 @@ import { ToastrService } from 'ngx-toastr'
   styleUrl: './contact-us.component.scss'
 })
 export class contactUsComponent {
-  latitude = 22.301267;
-  langitude = 70.816389;
+  latitude: number = 22.301267;
+  langitude: number = 70.816389;
 
   constructor(
     private readonly router: Router,
     private readonly toastr: ToastrService,
-  ) {
-
-  }
+  ) { }
 
   onSubmit(data: any) {
-    this.toastr.success('Your inquiry has been submitted successfully!')
-    this.router.navigate(['/']);
+    this.sendEmail(data);
   }
 
-  // redirectToGoogleMaps() {
-  //   const url = `https://www.google.com/maps?q=${this.latitude},${this.langitude}`;
-  //   window.open(url, '_blank'); // opens in new tab
-  // }
+  sendEmail(form: any) {
+    const templateParams = {
+      name: form.value?.name,
+      email: form.value?.email,
+      phone: form.value?.phone,
+      product: form.value?.product || 'Not specified',
+      title: form.value?.subject || 'No subject provided',
+      message: form.value?.message,
+      time: new Date().toLocaleString(),
+    };
+
+    emailjs.send(
+      'service_4y6qju4',
+      'template_rx59p89',
+      templateParams,
+      'Dfgj66wodoOAVH9jR'
+    ).then(
+      (response) => {
+        this.toastr.success('Your inquiry has been submitted successfully!')
+        this.router.navigate(['/']);
+        form.resetForm();
+      },
+      (error) => {
+        console.error('FAILED...', error);
+        this.toastr.error('Message failed to send.');
+      }
+    );
+
+  }
+
 }
